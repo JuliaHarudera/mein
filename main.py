8fb30166410dd121a170169457a59ecaff3aafdd
DMYTRO ORZHAKHOVSKYI <juliaharudera1@gmail.com>
2023-10-03 21:51:11 +0300

    Закончил с первыми 5 заданиями

--- main.py
+++ main.py
@@ -13,9 +13,49 @@
 # else:
 #     print("error")
 
-def count_unique_elements(imput_list):
-    unique_elements = set(imput_list)
-    return len(unique_elements)
-imput_list = [11,11,12,12,11,21, 2, 3 ,4 ,5 ,6 ,7]
-unique_elements = count_unique_elements(imput_list)
-print("Количество уникальных символов", unique_elements)
+# Task 2
+# def count_unique_elements(imput_list):
+#     unique_elements = set(imput_list)
+#     return len(unique_elements)
+# imput_list = [11,11,12,12,11,21, 2, 3 ,4 ,5 ,6 ,7]
+# unique_elements = count_unique_elements(imput_list)
+# print("Количество уникальных символов", unique_elements)
+
+# Task 3
+# def count_Unique_words(imput_list):
+#     Unique_words = set(imput_list)
+#     for meaning in imput_list:
+#         if meaning in Unique_words:
+#             return False
+#         Unique_words.add(meaning)
+#     return True
+# imput_list = {"bbb": "111",
+#               "aaa": "222",
+#               "ccc": "333"
+#               }
+# result = count_Unique_words(imput_list)
+# if result:
+#     print("Все значения соваря уникальные")
+# else:
+#     print("не все слова словаря есть уникальынми")
+
+# Task 4
+def find_common_friends(friendships, user1, user2):
+    if user1 in friendships and user2 in friendships:
+        common_friends = friendships[user1].intersection(friendships[user2])
+        return common_friends
+    else:
+        return set()
+friendships = {
+    "user1": {"user2", "user3", "user4"},
+    "user2": {"user1", "user3"},
+    "user3": {"user1", "user2", "user4"},
+    "user4": {"user1", "user3"}
+}
+user1 = "user1"
+user2 = "user2"
+common_friends = find_common_friends(friendships, user1, user2)
+if common_friends:
+    print(f"Спільні друзі користувачів {user1} і {user2}: {', '.join(common_friends)}")
+else:
+    print(f"У користувачів {user1} і {user2} немає спільних друзів.")
